8cb6e20e7b8b7952a458ce6ae18a7a3726ccbf5e
Omirzhan <omirzhan2@gmail.com>
2024-07-16 12:26:50 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+ "use strict";
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+
+console.log (incr);
+console.log (decr);
+
+console.log (5%2);
+console.log (2*4 == 8 );
+
+
+/* const answer = prompt ("вам есть  18", "18");
+console.log (answer); */
+
+/* const result = confirm ("are you here?");
+console.log (result) */
+
+
+/*alert ('hello'); */
+
+
+/* let number = 4.6;
+
+console.log (-4/0);
+console.log('string' * 9);
+
+const persone = `5`;
+const bool = true;
+
+let num = 100;
+num *= 15;
+console.log = (num);
+
+var num = 100;
+num *= 15;
+console.log(num);  */
+/* a = 15; 1 урок
+console.log(a);
+
+let number = 5;
+const LeftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+}
+
+obj.a = 10;
+
+console.log(obj); */
+
+/* const width = 5000; 2урок
+const length = 4000;
+
+console.log ("ширина кузова автомобиля:" + width +', длина:' + length ) */
+
